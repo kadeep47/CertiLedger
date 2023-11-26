f3ef362450717c8f09e6c2c9ef16393f5c8a0abc
kadeep47 <kadeep47@gmail.com>
2023-11-26 23:26:57 +0530

    added lock and ipfs feature

--- contracts/v2_ipfs.sol
+++ contracts/v2_ipfs.sol
@@ -8,6 +8,7 @@ contract Certification {
         string org_name;
         string course_name;
         uint256 expiration_date;
+        string ipfsLink;
     }
 
     mapping(bytes32 => Certificate) public certificates;
@@ -24,17 +25,17 @@ contract Certification {
         }
     }
 
-    function generateCertificate( string memory _id,string memory _candidate_name,string memory _org_name, string memory _course_name, uint256 _expiration_date) public {
+    function generateCertificate(string memory _id,string memory _candidate_name,string memory _org_name, string memory _course_name, uint256 _expiration_date,string memory _ipfsLink) public {
         bytes32 byte_id = stringToBytes32(_id);
         require(certificates[byte_id].expiration_date == 0, "Certificate with given id already exists");
-        certificates[byte_id] = Certificate(_candidate_name, _org_name, _course_name, _expiration_date);
+        certificates[byte_id] = Certificate(_candidate_name, _org_name, _course_name, _expiration_date, _ipfsLink);
         emit certificateGenerated(byte_id);
     }
 
-    function getData(string memory _id) public view returns(string memory, string memory, string memory, uint256) {
+    function getData(string memory _id) public view returns(string memory, string memory, string memory, uint256, string memory) {
         bytes32 byte_id = stringToBytes32(_id);
         Certificate memory temp = certificates[byte_id];
         require(temp.expiration_date != 0, "No data exists");
-        return (temp.candidate_name, temp.org_name, temp.course_name, temp.expiration_date);
+        return (temp.candidate_name, temp.org_name, temp.course_name, temp.expiration_date, temp.ipfsLink);
     }
 }
